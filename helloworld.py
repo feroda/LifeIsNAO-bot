7e04a13963133922050ae2f5e814bdd37301a318
Valerio Conti <valerio.conti@iismerlonimiliani.it>
2019-02-09 11:55:46 +0100

    Aggiunto titolo della chat nell'hello world

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,9 @@ from settings import TOKEN
 
 def hello(bot, update):
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hello {},sei in chat {}'.format(
+		update.message.from_user.first_name,
+		update.message.chat.title))
 
 
 updater = Updater(TOKEN)
@@ -14,4 +16,4 @@ updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
 updater.idle()
-
+ 
